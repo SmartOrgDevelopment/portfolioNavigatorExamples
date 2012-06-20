6191600283d8e1de6924d947f4043ee98cb2b29c
AgentIdea <GrantSteinfeld@gmail.com>
2012-06-20 18:21:34 -0400

    upload examples

---
+++ BG_example_CallingPNwebservice1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace BG_example_CallingPNwebservice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string usr = "admin";
+            string password = "smart";
+            string correlationID = "0112358";
+            string division = "Default Portfolio";
+            string projectDescription = " dummy description ";
+            string projectName = "dummy project C";
+            string templateName = "6dd";
+
+
+            ServiceReference1.ProjectSoapClient client = new BG_example_CallingPNwebservice1.ServiceReference1.ProjectSoapClient("ProjectSoap");
+            var response = client.CreateProject(usr, password, projectName, projectDescription, correlationID, division, templateName);
+
+            Console.WriteLine(response.Message);
+
+        }
+    }
+}
